55e46a9fc6372a2db8c93fa447c2396460ce11b2
Stuti Vithlani <me@stutivithlani.com>
2023-03-17 18:40:40 +0530

    changes made

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
-
-function validateForm(){
-  let id = document.querySelector("#id").value;
-  let pname = document.querySelector("#pname").value;
-  let price = document.querySelector("#price").value;
-  let photo = document.querySelector("#photo").value;
-  if(id==""){
-    alert("id required");
-    return false;    
-  }else if(isNaN(id)){
-    alert("Enter number in id")
-  }
-  if(pname==""){
-    alert("pname required");
-    return false;    
-  }
-  if(price==""){
-    alert("price required");
-    return false;    
-  }else if(isNaN(price)){
-    alert("Enter number in price")
-  }
-  if(photo==""){
-    alert("photo required");
-    return false;    
-  }
-  return true;
-}
- 
 let pId = document.getElementById('id');
 let pName = document.getElementById('pname');
 let pImage = document.getElementById('photo');
 let pPrice = document.getElementById('price');
 
+function validateForm(){
+    let id = document.querySelector("#id").value;
+    let pname = document.querySelector("#pname").value;
+    let price = document.querySelector("#price").value;
+    let photo = document.querySelector("#photo").value;
+    if(id==""){
+      alert("id required");
+      return false;    
+    }else if(isNaN(id)){
+      alert("Enter number in id")
+    }
+    if(pname==""){
+      alert("pname required");
+      return false;    
+    }
+    if(price==""){
+      alert("price required");
+      return false;    
+    }else if(isNaN(price)){
+      alert("Enter number in price")
+    }
+    if(photo==""){
+      alert("photo required");
+      return false;    
+    }
+    
+    return true;
+}
 
 function addData() {
-  if(validateForm() == true){
+  // if(validateForm() == true){
   var product;
   if (localStorage.getItem('productArray') == null) {
     product = [];
   } else {
     product = JSON.parse(localStorage.getItem('productArray'));
   }
+  let reader = new FileReader();
+  reader.readAsDataURL(pImage.files[0]);
+  reader.addEventListener('load', () => {
+  let photos = reader.result;
+  
   product.push({
     id: pId.value,
     name: pName.value,
     price: pPrice.value,
-    photo: pImage.value,
+    photo: photos,
     
   });
   
   localStorage.setItem('productArray', JSON.stringify(product));
 
   location.reload();
-}
+});
+// }
 }
 
 
@@ -70,15 +75,14 @@ function viewData(){
     }
     let html = "";
     product.forEach(function (element, index) {
-        html += `<tr>
-        <td>${element.id}</td>
-        <td>${element.name}</td>
-        <td>${element.price}</td>
-        <td><div style="width:100px; height:100px;"><img style="max-width: 100%; max-height:100%;" src="${element.photo}"/></div></td>
-        
-        <td><button type="button" class="btn btn-primary" onclick='updateData(${index})'><i class="fa fa-pencil" aria-hidden="true"></i></button></td>
-        <td><button type="button" class="btn btn-danger" onclick='deleteData(${index})'><i class="fa fa-trash" aria-hidden="true"></i></button></td>
-        </tr>`;
+        html += `<tr>`
+        html += `<td>${element.id}</td>`
+        html += `<td>${element.name}</td>`
+        html += `<td>${element.price}</td>`
+        html += `<td><div style="width:150px; height:100px;"><img style="max-width: 100%; max-height:100%;" src="${element.photo}"/></div></td>`
+        html += `<td><button type="button" class="btn btn-primary" onclick='updateData(${index})'><i class="fa fa-pencil" aria-hidden="true"></i></button></td>`
+        html += `<td><button type="button" class="btn btn-danger" onclick='deleteData(${index})'><i class="fa fa-trash" aria-hidden="true"></i></button></td>`
+        html += `</tr>`
 
 
     document.getElementById("product-list").innerHTML = html;
@@ -87,6 +91,7 @@ function viewData(){
     });
   
 }
+
 function deleteData(index){
   let product;
   if (localStorage.getItem("productArray") == null) {
@@ -95,7 +100,7 @@ function deleteData(index){
   else {
       product = JSON.parse(localStorage.getItem("productArray"));
   }
-  let deleted = confirm("Do you want to delete this product" + product[index.name] + "?");
+  let deleted = confirm("Do you want to delete this product " + product[index].name + "?");
   if(deleted == true){
     product.splice(index, 1)
     localStorage.setItem('productArray', JSON.stringify(product));
@@ -114,34 +119,45 @@ function updateData(index){
   else {
       product = JSON.parse(localStorage.getItem("productArray"));
   }
-  
-  document.getElementById('id').value = product[index].id;
-  document.getElementById('pname').value = product[index].name;
-  document.getElementById('price').value = product[index].price;
-  document.getElementById('photo').value = product[index].photo;
-
+  // let reader = new FileReader();
+  //   reader.readAsDataURL(pImage.files[0]);
+  //   reader.addEventListener('load', () => {
+  //   let photos = reader.result;
+    
+    document.getElementById('id').value = product[index].id;
+    document.getElementById('pname').value = product[index].name;
+    document.getElementById('price').value = product[index].price;
+    document.getElementById('photo').value = null;
+  // })
   document.querySelector("#update").onclick = function(){
-    if(validateForm() == true){
+    
+    // if(validateForm() == true){  
+    let reader = new FileReader();
+    reader.readAsDataURL(pImage.files[0]);
+    reader.addEventListener('load', () => {
+      let photos = reader.result;
       product[index].id = document.getElementById('id').value;
       product[index].name = document.getElementById('pname').value;
       product[index].price = document.getElementById('price').value;
-      product[index].photo = document.getElementById('photo').value;
+      product[index].photo = photos;
 
       localStorage.setItem('productArray', JSON.stringify(product));
 
       viewData();
 
-      document.getElementById('id').value = "";
-      document.getElementById('pname').value = "";
-      document.getElementById('price').value = "";
-      document.getElementById('photo').value = "";
+      // document.getElementById('id').value = "";
+      // document.getElementById('pname').value = "";
+      // document.getElementById('price').value = "";
+      // // document.getElementById('photo').value = "";
 
       document.getElementById('submit').style.display = "block";
       document.getElementById('update').style.display = "none";
-    }
+    })}
   }
+    
+  // }
 
-}
+
 
 function searchid(){
   var input, filter, table, tr, td, i, txtValue;
@@ -200,55 +216,40 @@ function asc(n){
       }
     }
   }
+  var down = document.getElementById('down');
+  var up = document.getElementById('up');
+  var down1 = document.getElementById('down1');
+  var up1 = document.getElementById('up1');
+  var down2 = document.getElementById('down2');
+  var up2 = document.getElementById('up2');
   if(dir == "asc" && n == 0){
-    document.getElementById('down').style.display = "none";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "inline";
+    down.style.display = "none";  up.style.display = "inline";
+    down1.style.display = "inline"; up1.style.display = "inline";
+    down2.style.display = "inline"; up2.style.display = "inline";
   }else if(dir == "desc" && n==0){
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "none";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "inline";
+    down.style.display = "inline";  up.style.display = "none";
+    down1.style.display = "inline"; up1.style.display = "inline";
+    down2.style.display = "inline"; up2.style.display = "inline";
 }else if(dir == "asc" && n==1){
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "none";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "inline";
+    down.style.display = "inline";  up.style.display = "inline";
+    down1.style.display = "none"; up1.style.display = "inline";
+    down2.style.display = "inline"; up2.style.display = "inline";
 }else if(dir == "desc" && n==1){
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "none";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "inline";
+    down.style.display = "inline";  up.style.display = "inline";
+    down1.style.display = "inline"; up1.style.display = "none";
+    down2.style.display = "inline"; up2.style.display = "inline";
 }else if(dir == "asc" && n==2){
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "none";
-    document.getElementById('up2').style.display = "inline";
+    down.style.display = "inline";  up.style.display = "inline";
+    down1.style.display = "inline"; up1.style.display = "inline";
+    down2.style.display = "none"; up2.style.display = "inline";
 }else if(dir == "desc" && n==2){
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "none";
+    down .style.display= "inline";  up.style.display = "inline";
+    down1.style.display = "inline"; up1.style.display = "inline";
+    down2.style.display = "inline"; up2.style.display = "none";
   }else{
-    document.getElementById('down').style.display = "inline";
-    document.getElementById('up').style.display = "inline";
-    document.getElementById('down1').style.display = "inline";
-    document.getElementById('up1').style.display = "inline";
-    document.getElementById('down2').style.display = "inline";
-    document.getElementById('up2').style.display = "inline";    
+    down.style.display = "inline";  up.style.display = "inline";
+    down1.style.display = "inline"; up1.style.display = "inline";
+    down2.style.display = "inline"; up2.style.display = "inline";    
   }     
 }
 
